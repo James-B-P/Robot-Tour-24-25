49600be968850efd8774677c4f505fa43157f5e4
James-B-P <jamesbp.tablet@gmail.com>
2024-12-12 23:07:30 -0800

    Commented Code Submission

--- RETRO_PULSE_WORKING.ino
+++ RETRO_PULSE_WORKING.ino
@@ -1,5 +1,9 @@
-// Instructions
-const int INSTRUCTIONS_LEN = 10;
+// ROBOT INSTRUCTIONS: Should be the only modified part during competitions. Path is programmed
+// as a matrix of points it passes. Currently, adjacent points must share either an x value or
+// a y value.
+// INSTRUCTIONS_LEN must be equal to the number of points in the instructions matrix.
+const int INSTRUCTIONS_LEN = 10; 
+// Example matrix of instructions to trace a figure eight:
 const int instructions[INSTRUCTIONS_LEN][2] = 
 {
   {  0,    0},
@@ -13,6 +17,10 @@ const int instructions[INSTRUCTIONS_LEN][2] =
   {-16,    0},
   {  0,    0}
 };
+// Robot's starting position and orientation
+volatile int robot_direction = 0;
+volatile int robot_x = 0;
+volatile int robot_y = 0;
 
 // Pin Constants
 const int MLE = 7;
@@ -31,23 +39,22 @@ const float K_I = 0.000005;
 const float K_D = 250;
 const int DELAY = 50;
 const float L2R_RATIO = 0.7;
+const float TICKS_PER_CM = 1;
 
 // Pulse constants
 const int PULSE_POWER = 255;
 const int PULSE_DELAY = 50;
 
-// Movement constants
-int TIME = 30000;
-float TPMS = .017;
-
+// Motor class: Used to simplify interactions between program and physical motors
 class Motor
 {
   public:
-    Motor(int InA, int InB, int En, int Interrupt):InA(InA), InB(InB), En(En), Interrupt(Interrupt){};
+    Motor(int InA, int InB, int En):InA(InA), InB(InB), En(En){};
     volatile float power = 0;
     volatile int ticks = 0;
     volatile float prev_power = 1;
 
+    // Updates signal to L298N
     void run()
     {
       analogWrite(En, (int)(power));
@@ -83,18 +90,13 @@ class Motor
     const int InA;
     const int InB;
     const int En;
-    const int Interrupt;
 };
 
 // Motor setup
-Motor left_motor(MLA, MLB, MLE, INL);
-Motor right_motor(MRA, MRB, MRE, INR);
-volatile int robot_direction = 0;
-volatile int robot_x = 0;
-volatile int robot_y = 0;
+Motor left_motor(MLA, MLB, MLE);
+Motor right_motor(MRA, MRB, MRE);
 
-volatile bool paused = true;
-
+// Tracks wheel encoder ticks
 void incL()
 {
   if (left_motor.power == 0)
@@ -117,18 +119,6 @@ void incR()
   }
 }
 
-
-void startProgram()
-{
-  paused = false;
-}
-
-void p(float v)
-{
-  Serial.print(v);
-  Serial.print(",");
-}
-
 int left_ticks()
 {
   return left_motor.ticks;
@@ -139,6 +129,8 @@ int right_ticks()
   return right_motor.ticks;
 }
 
+// Gives a short pulse to the motors to either quickly slow down or speed up
+//   • left_direction and right_direction are ints either -1 or 1
 void retro_pulse(int left_direction, int right_direction)
 {
   left_motor.set_power(left_direction*PULSE_POWER);
@@ -148,7 +140,12 @@ void retro_pulse(int left_direction, int right_direction)
   right_motor.set_power(0);
 }
 
-void move_PID(int left_direction, int right_direction, int avg_power, float total_ticks)
+// Using PID motor control, rotates motors in a given direction for a given length
+//   • left_direction and right_direction are ints either -1 or 1
+//   • avg_power is an int between 0 and 255
+//   • total_ticks is a positive int corresponding to the total number of wheel encoder 
+//     ticks counted in the movement
+void move_PID(int left_direction, int right_direction, int avg_power, int total_ticks)
 {
   int avg_ticks;
   int tick_dif;
@@ -168,13 +165,10 @@ void move_PID(int left_direction, int right_direction, int avg_power, float tota
 
   while (avg_ticks < total_ticks)
   {
-    // Tick update
+    // Encoder ticks update
     avg_ticks = (left_ticks()*left_direction+right_ticks()*right_direction)/2;
-    p(left_motor.power);
-    p(right_motor.power);
-    Serial.println();
     
-    // PID
+    // PID control
     tick_dif = left_direction*left_ticks()-right_direction*right_ticks();
     accum_tick_dif += tick_dif;
     PID = K_P*tick_dif + K_I*accum_tick_dif*DELAY + K_D*(tick_dif-prev_tick_dif)/DELAY;
@@ -189,9 +183,9 @@ void move_PID(int left_direction, int right_direction, int avg_power, float tota
   retro_pulse(-left_direction, -right_direction);  
 }
 
-void forward(int ticks)
+void forward(int cm)
 {
-  move_PID(1, 1, 100, ticks);
+  move_PID(1, 1, 100, (int)(TICKS_PER_CM*cm));
   delay(200);
 }
 
@@ -207,6 +201,9 @@ void turn(int degrees_clockwise)
   delay(200);
 }
 
+// Lines robot up for next movement
+//   • new_direction is an int between 0 and 3 with each value corresponding
+//     to a different direction by increments of 90°
 void rotate_to(int new_direction)
 {
   int dir_change = (new_direction-robot_direction+2)%4-2;
@@ -217,27 +214,32 @@ void rotate_to(int new_direction)
   robot_direction += dir_change;
 }
 
+volatile bool paused = true;
+
+void startProgram()
+{
+  paused = false;
+}
 
 void setup()
 {
+  // General setup and wait for button press to start program
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(INL), incL, RISING);
   attachInterrupt(digitalPinToInterrupt(INR), incR, RISING);
   attachInterrupt(digitalPinToInterrupt(BUTTON), startProgram, FALLING);
   while (paused){}
-  delay(500);
-}
 
-void loop()
-{
-  delay(500);
+  delay(1000);
   int x_move;
   int y_move;
 
+  // Iterate through instructions matrix and move accordingly
   for (int i = 0; i < INSTRUCTIONS_LEN; i++)
   {
     x_move = instructions[i][0]-robot_x;
     y_move = instructions[i][1]-robot_y;
+
     if (x_move > 0)
     {
       rotate_to(1);
@@ -258,6 +260,9 @@ void loop()
     robot_x += x_move;
     robot_y += y_move;
   }
-  
-  while (true){}
+}
+
+void loop()
+{
+
 }
